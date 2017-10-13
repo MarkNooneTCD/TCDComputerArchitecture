1498655ad7d473123abd3f557033d80e17d34a18
Mark Noone <manoone@tcd.ie>
2017-10-13 15:16:30 +0100

    Added the Lab-002 project for visual studio

---
+++ Lab-002/Lab-002/t2.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//
+// fib32.h
+//
+// Copyright (C) 2012 - 2017 jones@scss.tcd.ie
+//
+// example of mixing C++ and IA32 assembly language
+//
+
+//
+// NB: "extern C" to avoid procedure name mangling by compiler
+//
+
+extern "C" _int64 minx64(_int64, _int64, _int64);
+//extern "C" _int64 p(_int64, _int64, _int64, _int64);
+//extern "C" _int64 gcd(_int64, _int64);
+//extern "C" _int64 q(_int64, _int64, _int64, _int64, _int64);
+
+// eof
