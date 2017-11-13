19b94f430783514045622555933f3f99a2aa1921
MarkNooneTCD <manoone@tcd.ie>
2017-11-13 09:23:35 +0000

    Added Report for tutorial 3

--- Reports/tutoria-03l.s
+++ Reports/tutoria-03l.s
@@ -2,18 +2,18 @@
 
 		add		r0, #4, r3
 
-min:	add		r0, r26, r1
+min:		add		r0, r26, r1
 		sub		r27, r16, r0{C}
 		jge		min1
 		xor 	r0, r0, r0
 		add		r0, r27, r1
 		
-min1:	sub		r0, r28, r1
+min1:		sub		r0, r28, r1
 		jge		min2
 		xor 	r0, r0, r0
 		add		r0, r28, r1
 		
-min2:	ret		r25, r0
+min2:		ret		r25, r0
 		xor 	r0, r0, r0
 
 		
@@ -53,18 +53,18 @@ gdc1:	add		r0, r27, r11
 
 add		r0, #4, r3
 
-min:	add		r0, r26, r1
+min:		add		r0, r26, r1
 		sub		r27, r16, r0{C}
 		jge		min1
 		xor 	r0, r0, r0
 		add		r0, r27, r1
 		
-min1:	sub		r0, r28, r1
+min1:		sub		r0, r28, r1
 		jge		min2
 		xor 	r0, r0, r0
 		add		r0, r28, r1
 		
-min2:	ret		r25, r0
+min2:		ret		r25, r0
 		xor 	r0, r0, r0
 
 		
@@ -80,12 +80,12 @@ p:		add 	r0, r3, r10
 		xor 	r0, r0, r0
 		
 		
-gcd:	sub		r27, r0, r0{C}
+gcd:		sub		r27, r0, r0{C}
 		jeq		gcd1
 		xor		r0, r0, r0
 		ret		r25, r0
 		add 	r0, r26, r1
-gdc1:	add		r0, r27, r11
+gdc1:		add		r0, r27, r11
 		callr	r25, mod
 		add		r0, r26, r10
 		add		r0, r1, r11
